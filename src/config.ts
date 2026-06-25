2757d8fbd54873b54f5cc561a486c8b1704e1639
Chris Bolson <chris@cbolson.com>
2026-06-25 11:02:39 +0200

    Update site metadata and author information

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "I Digress";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
-export const MY_NAME = "Captain Awesome";
+  "musings and observations, mostly about CSS";
+export const X_HANDLE = "@cbolson";
+export const bsky_HANDLE = "@cbolson";
+export const MY_NAME = "Chris Bolson";
 
 // setup in astro.config.mjs
 const BASE_URL = new URL(import.meta.env.SITE);
